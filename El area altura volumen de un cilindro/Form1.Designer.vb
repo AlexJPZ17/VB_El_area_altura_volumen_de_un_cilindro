9123e533303881960c0f709dfc030ddef574f96d
AlexJPZ17 <alexjpz17@gmail.com>
2014-01-04 11:50:03 -0600

    VB El area altura volumen de un cilindro

---
+++ El area altura volumen de un cilindro/Form1.Designer.vb
@@ -0,0 +1,198 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label_Area = New System.Windows.Forms.Label()
+        Me.Label_Volumen = New System.Windows.Forms.Label()
+        Me.TextBox_Radio = New System.Windows.Forms.TextBox()
+        Me.TextBox_Altura = New System.Windows.Forms.TextBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button_Calcular = New System.Windows.Forms.Button()
+        Me.Button_Limpiar = New System.Windows.Forms.Button()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.18868!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(12, 386)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 20)
+        Me.Label4.TabIndex = 35
+        Me.Label4.Text = "Volumen:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.18868!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(41, 345)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(47, 20)
+        Me.Label3.TabIndex = 34
+        Me.Label3.Text = "Area:"
+        '
+        'Label_Area
+        '
+        Me.Label_Area.AutoSize = True
+        Me.Label_Area.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.18868!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Area.Location = New System.Drawing.Point(94, 345)
+        Me.Label_Area.Name = "Label_Area"
+        Me.Label_Area.Size = New System.Drawing.Size(18, 20)
+        Me.Label_Area.TabIndex = 33
+        Me.Label_Area.Text = "0"
+        '
+        'Label_Volumen
+        '
+        Me.Label_Volumen.AutoSize = True
+        Me.Label_Volumen.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.18868!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_Volumen.Location = New System.Drawing.Point(94, 386)
+        Me.Label_Volumen.Name = "Label_Volumen"
+        Me.Label_Volumen.Size = New System.Drawing.Size(18, 20)
+        Me.Label_Volumen.TabIndex = 32
+        Me.Label_Volumen.Text = "0"
+        '
+        'TextBox_Radio
+        '
+        Me.TextBox_Radio.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_Radio.Location = New System.Drawing.Point(207, 311)
+        Me.TextBox_Radio.Name = "TextBox_Radio"
+        Me.TextBox_Radio.Size = New System.Drawing.Size(74, 22)
+        Me.TextBox_Radio.TabIndex = 31
+        '
+        'TextBox_Altura
+        '
+        Me.TextBox_Altura.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_Altura.Location = New System.Drawing.Point(341, 183)
+        Me.TextBox_Altura.Name = "TextBox_Altura"
+        Me.TextBox_Altura.Size = New System.Drawing.Size(74, 22)
+        Me.TextBox_Altura.TabIndex = 30
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(358, 14)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(147, 143)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 29
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 14)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(336, 273)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 28
+        Me.PictureBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.18868!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(221, 288)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 20)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "Radio"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.18868!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(354, 160)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 20)
+        Me.Label1.TabIndex = 26
+        Me.Label1.Text = "Altura"
+        '
+        'Button_Calcular
+        '
+        Me.Button_Calcular.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_Calcular.Location = New System.Drawing.Point(113, 428)
+        Me.Button_Calcular.Name = "Button_Calcular"
+        Me.Button_Calcular.Size = New System.Drawing.Size(75, 25)
+        Me.Button_Calcular.TabIndex = 25
+        Me.Button_Calcular.Text = "Calcular"
+        Me.Button_Calcular.UseVisualStyleBackColor = True
+        '
+        'Button_Limpiar
+        '
+        Me.Button_Limpiar.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.830189!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_Limpiar.Location = New System.Drawing.Point(299, 428)
+        Me.Button_Limpiar.Name = "Button_Limpiar"
+        Me.Button_Limpiar.Size = New System.Drawing.Size(75, 25)
+        Me.Button_Limpiar.TabIndex = 24
+        Me.Button_Limpiar.Text = "Limpiar"
+        Me.Button_Limpiar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(517, 466)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label_Area)
+        Me.Controls.Add(Me.Label_Volumen)
+        Me.Controls.Add(Me.TextBox_Radio)
+        Me.Controls.Add(Me.TextBox_Altura)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button_Calcular)
+        Me.Controls.Add(Me.Button_Limpiar)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "El area altura volumen de un cilindro"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label_Area As System.Windows.Forms.Label
+    Friend WithEvents Label_Volumen As System.Windows.Forms.Label
+    Friend WithEvents TextBox_Radio As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox_Altura As System.Windows.Forms.TextBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button_Calcular As System.Windows.Forms.Button
+    Friend WithEvents Button_Limpiar As System.Windows.Forms.Button
+
+End Class
---
+++ El area altura volumen de un cilindro/Form1.vb
@@ -0,0 +1,49 @@
+﻿Public Class Form1
+    Private Sub Cilindro()
+        'Declaramos 5 variables de tipo double
+        Dim altura, area, radio, radio2, volumen As Double
+        'Condición que valida los TextBox si  estos están vacío 
+        If TextBox_Altura.Text = "" Then
+            MessageBox.Show("Ingrese una altura", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            'Coloca el Focus en el TextBox_Altura
+            TextBox_Altura.Focus()
+        Else
+            'Condición que valida los TextBox si  estos están vacío 
+            If TextBox_Radio.Text = "" Then
+                MessageBox.Show("Ingrese un radio", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                'Coloca el Focus en el TextBox_Radio
+                TextBox_Radio.Focus()
+            Else
+                'Funciones que convierte en un tipo double el valor que se ingrese por 
+                'los TextBox y lo almacena en las variables altura, radio 
+                altura = Convert.ToDouble(TextBox_Altura.Text)
+                radio = Convert.ToDouble(TextBox_Radio.Text)
+                'Método para calcular el area del cilindro
+                area = (2 * Math.PI * radio * (altura + radio))
+                ' Eleva al cuadrado el dato almacenado en las variable 
+                ' radio y loa almacena  en la variable radio2
+                radio2 = Math.Pow(radio, 2)
+                'Método para calcular el volumen del cilindro
+                volumen = (Math.PI * radio2 * altura)
+                'Mostrar el resultado que esta en las variables area, volumen 
+                Label_Area.Text = area.ToString()
+                Label_Volumen.Text = volumen.ToString()
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Button_Limpiar_Click(sender As Object, e As EventArgs) Handles Button_Limpiar.Click
+        'Restablecer los controles 
+        TextBox_Altura.Text = ""
+        TextBox_Radio.Text = ""
+        Label_Area.Text = "0"
+        Label_Volumen.Text = "0"
+        TextBox_Altura.Focus()
+    End Sub
+
+    Private Sub Button_Calcular_Click(sender As Object, e As EventArgs) Handles Button_Calcular.Click
+        Cilindro()
+    End Sub
+End Class
+
